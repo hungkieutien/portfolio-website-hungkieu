392213aff72e70a9b7c5af16a7fc02b31743b97e
Hung Kieu <hung.kieu@netgrade.de>
2023-10-02 10:52:36 +0200

    Footer fertiggestellt

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,35 @@
 import React from "react";
+import "../styles/components/_footer.scss";
+import {IconBrandGithub, IconBrandLinkedin} from "@tabler/icons-react";
 
 function Footer() {
     return (
         <>
+            <footer>
+                <div className="container">
+                    <div className="footer-wrap">
+                        <h3>Copyright © 2023, Built by Hung Kieu</h3>
+                        <div className="footer-links">
+                            <span>
+                            <a
+                                aria-label="github"
+                                rel="noreferrer"
+                                target="_blank"
+                                href="https://github.com/hungkieutien">
+                                <IconBrandGithub width={32} height={32}/>
+                            </a>
+                            <a
+                                aria-label="linkedin"
+                                rel="noreferrer"
+                                target="_blank"
+                                href="https://www.linkedin.com/in/hung-kieu-tien/">
+                                <IconBrandLinkedin width={32} height={32}/>
+                            </a>
+                        </span>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </>
     );
 }
